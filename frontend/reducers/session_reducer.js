b668eaf8c9d09509b20e1e5be7876e610da6ab71
rlachivirus <rlachivirus@gmail.com>
2021-10-29 20:16:59 -0400

    done with reducers

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const _nullUser = Object.freeze({
+    id: null
+});
+
+const sessionReducer = (state = _nullUser, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            nextState[action.currentUser.id] = action.currentUser;
+            return nextState;
+        case LOGOUT_CURRENT_USER:
+            return _nullUser;
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
--- frontend/webook.jsx
+++ frontend/webook.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login, signUp, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Hi!</h1>, root);
+    // window.login = login;
+    // window.signUp = signUp;
+    // window.logout = logout;
 })
