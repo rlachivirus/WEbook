c59ebeba8a76186b157aa7065698815ebd33987a
rlachivirus <rlachivirus@gmail.com>
2021-12-05 18:31:10 -0500

    got the post-top styled

--- frontend/components/post/post.jsx
+++ frontend/components/post/post.jsx
@@ -54,48 +54,61 @@ class Post extends React.Component {
                 <div className="newsfeed-left"></div>
                 <div className="newsfeed-middle">
                     <div className="create-post">
-                        <div onClick={() => this.openModal({ type: 'createPost', currentUserId: this.props.currentUserId, userId: this.props.userId, createPost: this.props.createPost, currentUser: this.props.currentUser })}>{`What's on your mind, ${this.props.currentUser.fname}?`}</div>
+                        <div className="create-button" onClick={() => this.openModal({ type: 'createPost', currentUserId: this.props.currentUserId, userId: this.props.userId, createPost: this.props.createPost, currentUser: this.props.currentUser })}>{`What's on your mind, ${this.props.currentUser.fname}?`}</div>
                     </div>
                     {/* <div className="newsfeed-posts"> */}
-                        <ul className="newsfeed-posts">
-                            {Object.values(posts).reverse().map(post => {
-                                // console.log(this.props.users[post.user_id])
-                                if (friendIds.includes(post.author_id)) {
-                                    let months = { 1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December" };
-                                    let currentYear = this.state.date;
-                                    let postYear = parseInt(post.created_at.slice(0, 10).split("-")[0]) !== currentYear ? `, ${post.created_at.slice(0, 10).split("-")[0]}` : "";
-                                    let postMonth = months[post.created_at.slice(0, 10).split("-")[1]];
-                                    let postDay = post.created_at.slice(0, 10).split("-")[2];
-                                    let postName;
+                    <ul className="newsfeed-posts">
+                        {Object.values(posts).reverse().map(post => {
+                            // console.log(this.props.users[post.user_id])
+                            if (friendIds.includes(post.author_id)) {
+                                let months = { 1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December" };
+                                let currentYear = this.state.date;
+                                let postYear = parseInt(post.created_at.slice(0, 10).split("-")[0]) !== currentYear ? `, ${post.created_at.slice(0, 10).split("-")[0]}` : "";
+                                let postMonth = months[post.created_at.slice(0, 10).split("-")[1]];
+                                let postDay = post.created_at.slice(0, 10).split("-")[2];
+                                let postName;
 
-                                    if (this.props.users[post.user_id]) {
-                                        // postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : post.user_id === 0 ? `${post.fname} ${post.lname}` : `${post.fname} ${post.lname} TO ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}`
-                                        postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : `${post.fname} ${post.lname} ▶ ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}`
-                                    } else {
-                                        // postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : post.user_id ? `${post.fname} ${post.lname} TO ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}` : null
-                                        postName = `${post.fname} ${post.lname}`
-                                    }
-
-                                    return (
-                                        // <div key={post.id}>
-                                        // <div id={post.id} className="newsfeed-posts">
-                                            <li id={post.id} className="post">
-                                                {/* <span id={post.id}></span> */}
-                                                {/* <span className="post-name">{`${post.fname} ${post.lname}`}</span> */}
-                                                <img className="profile-picture" src={this.props.users[post.author_id].photoUrl} />
-                                                <span className="post-name">{postName}</span>
-                                                <span>{`${postMonth} ${postDay}${postYear}`}</span>
-                                                <br/>
-                                                <span className="post-body">{post.body}</span>
-                                                <br/>
-                                                <img className="post-picture" src={post.photoUrl} />
-                                                <PostEditButton id={post.id} />
-                                            </li>
-                                        // </div>
-                                    )
+                                if (this.props.users[post.user_id]) {
+                                    // postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : post.user_id === 0 ? `${post.fname} ${post.lname}` : `${post.fname} ${post.lname} TO ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}`
+                                    postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : `${post.fname} ${post.lname} ▶ ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}`
+                                } else {
+                                    // postName = post.author_id === post.user_id ? `${post.fname} ${post.lname}` : post.user_id ? `${post.fname} ${post.lname} TO ${this.props.users[post.user_id].fname} ${this.props.users[post.user_id].lname}` : null
+                                    postName = `${post.fname} ${post.lname}`
                                 }
-                            })}
-                        </ul>
+
+                                return (
+                                    // <div key={post.id}>
+                                    // <div id={post.id} className="newsfeed-posts">
+                                        <li id={post.id} className="post">
+                                            {/* <span id={post.id}></span> */}
+                                            {/* <span className="post-name">{`${post.fname} ${post.lname}`}</span> */}
+                                            <div className="post-top">
+                                                <div className="post-top-left">
+                                                    <img className="profile-picture" src={this.props.users[post.author_id].photoUrl} />
+                                                    <div className="post-nameAndDate">
+                                                        <span className="post-name">{postName}</span>
+                                                        <span>{`${postMonth} ${postDay}${postYear}`}</span>
+                                                    </div>
+                                                </div>
+                                                <PostEditButton id={post.id} />
+                                            </div>
+                                            <br/>
+                                            <span className="post-body">{post.body}</span>
+                                            <br/>
+                                            <img className="post-picture" src={post.photoUrl} />
+                                            <hr/>
+                                            <div>
+                                                <div>Like</div>
+                                                <div>Comment</div>
+                                            </div>
+                                            <hr/>
+                                            <div>Write a Comment...</div>
+                                        </li>
+                                    // </div>
+                                )
+                            }
+                        })}
+                    </ul>
                     {/* </div> */}
                 </div>
                 <div className="newsfeed-right"></div>
